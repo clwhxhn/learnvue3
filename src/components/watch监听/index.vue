8a4a8900849877549ec4ec1cd272916acad83216
wangxiaohui <clwhxhn@163.com>
2022-12-15 15:44:22 +0800

    1

--- src/components/watch监听/index.vue
+++ src/components/watch监听/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: clwhxhn clwhxhn@163.com
+ * @Date: 2022-11-06 17:42:02
+ * @LastEditors: clwhxhn clwhxhn@163.com
+ * @LastEditTime: 2022-11-21 11:00:05
+ * @FilePath: /learnvue3/src/components/watch监听/index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div>
     我是父000组件- {{ info }}
@@ -10,9 +18,16 @@
 <script>
 import { provide, ref, reactive } from "vue";
 import myChild from "./myChild";
+import { getCurrentInstance } from 'vue'
+
 export default {
   components: { myChild },
   setup() {
+
+
+    const instance = getCurrentInstance()
+    console.log(instance.appContext.config.globalProperties.name + '----');
+
     const info = ref(10);
 
     provide("info", info); // key , value
@@ -27,6 +42,9 @@ export default {
       add,
     };
   },
+  mounted() {
+    alert(this.name)
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
+/*
+ * @Author: wangxiaohui clwhxhn@163.com
+ * @Date: 2022-09-24 20:12:48
+ * @LastEditors: wangxiaohui clwhxhn@163.com
+ * @LastEditTime: 2022-12-12 15:59:47
+ * @FilePath: /learnvue3/src/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
+import myPlugin from './components/plugins/对象写法/index.js'
+import chalk from 'chalk'
+
+
+console.log(chalk.blue('Hello world!'));
 
 // createApp(App).mount('#app')
 
 //mixin全局混入
 const app = createApp(App)
+
+app.use(myPlugin)
+
 app.mixin({
     created() {
         console.log('全局混入: ');
