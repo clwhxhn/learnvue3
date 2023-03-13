29947e9d70f0ad0b4010cc3f8536940b28acd2e1
wangxiaohui <clwhxhn@163.com>
2023-03-13 15:54:45 +0800

    -0

--- vue3-ts-cms/src/App.vue
+++ vue3-ts-cms/src/App.vue
@@ -1,16 +1,21 @@
+<!--
+ * @Author: wangxiaohui clwhxhn@163.com
+ * @Date: 2023-03-10 17:44:58
+ * @LastEditors: wangxiaohui clwhxhn@163.com
+ * @LastEditTime: 2023-03-13 15:52:17
+ * @FilePath: \learnvue3\vue3-ts-cms\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
   },
 })
 </script>
--- vue3-ts-cms/vue.config.js
+++ vue3-ts-cms/vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: './build',
+  // 方式一
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components',
+  //     },
+  //   },
+  // },
+
+  // 方式二
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve('__dirname', 'src'),
+  //     components: '@/components',
+  //   }
+  // },
+
+  //方式三(链式)
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', path.resolve('__dirname', 'src')).set('components', '@/components')
+  },
 })
