569f7f7ead9206ad4c55b51e16b1e7cb967c6195
wangxiaohui <clwhxhn@163.com>
2023-03-13 11:18:33 +0800

    1

--- vue3-ts-cms/src/main.ts
+++ vue3-ts-cms/src/main.ts
@@ -1,4 +1,15 @@
+/*
+ * @Author: wangxiaohui clwhxhn@163.com
+ * @Date: 2023-03-10 17:44:58
+ * @LastEditors: wangxiaohui clwhxhn@163.com
+ * @LastEditTime: 2023-03-13 11:18:28
+ * @FilePath: \learnvue3\vue3-ts-cms\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+
 
 createApp(App).mount('#app')
