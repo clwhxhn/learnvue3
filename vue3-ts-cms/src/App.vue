b79d1f159da98f23022c9b53f62fe16e5a489044
wangxiaohui <clwhxhn@163.com>
2023-03-14 14:56:46 +0800

    课堂资料

--- vue3-ts-cms/src/App.vue
+++ vue3-ts-cms/src/App.vue
@@ -2,6 +2,7 @@
   <div class="app">
     <router-link to="/login">login</router-link>
     <router-link to="/main">main</router-link>
+    {{ $store.state.name }}
     <router-view></router-view>
   </div>
 </template>
